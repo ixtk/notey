53394d6a2295b5921d8e0e61701ffa4ab07c07be
ixtk <ikhcamot@gmail.com>
2026-04-07 07:59:29 +0400

    Simple timers practice

---
+++ timers.js
@@ -0,0 +1,15 @@
+// let time = 5
+
+// const intervalId = setInterval(function() {
+//   console.log(time)
+//   time = time - 1
+
+//   if (time === 0) {
+//     clearInterval(intervalId)
+//     console.log("Time passed!")
+//   }
+// }, 1000)
+
+setTimeout(function() {
+  console.log("Hello")
+}, 3000)
